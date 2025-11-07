31d6a4b25b9b273b72fb95bad5d9719dd36e4f96
leezisback <aailichenko@gmail.com>
2025-11-07 12:46:18 +0600

    Первоначальный коммит фронт структура

---
+++ front/src/App.jsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+import Header from "@/components/layout/Header.jsx";
+import HomePage from "@/pages/Home";
+import MenuPage from "@/pages/Menu";
+import PromotionsPage from "@/pages/Promotions";
+import CheckoutPage from "@/pages/Checkout";
+import ContactsPage from "@/pages/Contacts";
+import AdminPage from "@/pages/Admin";
+
+export default function App() {
+    return (
+        <div className="min-h-dvh flex flex-col">
+            <Header />
+            <main className="flex-1">
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/menu" element={<MenuPage />} />
+                    <Route path="/promotions" element={<PromotionsPage />} />
+                    <Route path="/checkout" element={<CheckoutPage />} />
+                    <Route path="/contacts" element={<ContactsPage />} />
+                    <Route path="/admin" element={<AdminPage />} />
+                </Routes>
+            </main>
+        </div>
+    );
+}
